ddcfc608d29c0c5b0816cd510e769e1b12857e43
swapnilds23 <swapnilds23@gmail.com>
2018-04-23 22:24:11 -0700

    .

---
+++ library-app/app/controllers/index.js
@@ -0,0 +1,23 @@
+import {
+  match,
+  not
+} from '@ember/object/computed';
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+  emailAddress: '',
+
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+
+});
